fcd732877eb1e758588dcbc1d078a9b3333fadc5
Lukas412 <lukas.weber.4.12@gmail.com>
2023-10-19 14:40:15 +0200

    update bash files

--- setup.sh
+++ setup.sh
@@ -2,3 +2,9 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Create SymLinks
 ln -s "$script_dir"/ideavimrc.symlink ~/.ideavimrc
+ln -s "$script_dir"/bashrc.symlink ~/.bashrc
+ln -s "$script_dir"/bash_aliases.symlink ~/.bash_aliases
+ln -s "$script_dir"/bash_logout.symlink ~/.bash_aliases
+ln -s "$script_dir"/vimrc.symlink ~/.vimrc
+
+echo "symlinks created!"
