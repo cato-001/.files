c3648482e913f411ef3250bdd555ed58e27cf765
Lukas Weber <lukas.weber.4.12@gmail.com>
2024-01-02 19:01:38 +0100

    update

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ ln -s "$script_dir"/bashrc.symlink ~/.bashrc
 ln -s "$script_dir"/bash_aliases.symlink ~/.bash_aliases
 ln -s "$script_dir"/bash_logout.symlink ~/.bash_aliases
 ln -s "$script_dir"/vimrc.symlink ~/.vimrc
+ln -s "$script_dir"/tmux.conf ~/.tmux.conf
 
 echo "symlinks created!"
