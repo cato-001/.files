7cd02edc6b1c62661e1bf36a12d7f9a9e967bb8d
Lukas412 <lukas.weber.4.12@gmail.com>
2024-05-30 18:23:42 +0200

    update uml plugins

--- .config/nvim/lua/custom/plugins/uml.lua
+++ .config/nvim/lua/custom/plugins/uml.lua
@@ -1,7 +1,8 @@
-return {
-  'https://gitlab.com/itaranto/plantuml.nvim',
-  version = '*',
-  config = function()
-    require('plantuml').setup()
-  end,
-}
+local function open_plantuml_file_in_browser(event)
+  local file = event['file'] or vim.fn.expand('%')
+  vim.fn.system({ 'puml', 'open', file })
+end
+
+vim.api.nvim_create_user_command('Puml', open_plantuml_file_in_browser, {})
+
+return {}
